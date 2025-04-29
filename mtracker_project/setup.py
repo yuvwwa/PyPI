d3e3cf68b522f4e87a3c26fb43587419e0c8a460
yuvwwa <yuvwwa@gmail.com>
2025-04-29 21:17:04 +0800

    add project

---
+++ mtracker_project/setup.py
@@ -0,0 +1,19 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='mtracker_project_yulya',
+    version='1.0.0',
+    description='Provides a decorator for memory usage tracking. The part of FOSS course.',
+    license='MIT',
+    long_description=open('README.md').read(),
+    long_description_content_type='text/markdown',
+    author='Yulia Kravtsova',
+    author_email='yuvwwa@gmail.com',
+    url='https://github.com/yuvwwa/PyPI.git',
+    packages=find_packages(),
+    install_requires=[],
+    extras_require={
+        'test': ['pytest', 'coverage']
+    },
+    python_requires='>=3.6',
+)
